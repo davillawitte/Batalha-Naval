26e0fbe9ce6bfe9b893706a607f516222db6e6ba
davillawitte <30900829+davillawitte@users.noreply.github.com>
2020-03-10 21:53:21 -0300

    Alocado matriz dinamicamente
    Começando a implementar a função 'Jogador'

--- naval.cpp
+++ naval.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 const int MAX = 10;
 
-void Tabuleiro (){
+void Tabuleiro (int **&matriz_tab){
     
     /*  5 quadrados = porta-avioes
         4 quadrados = navio de guerra
@@ -12,9 +12,14 @@ void Tabuleiro (){
         2 quadrados = destruidor
         */
     int i,j;
-    int matriz_tab[10][10];
+    matriz_tab = new int*[10];
 
     cout << "Gerando Tabuleiro..." << endl << endl;
+
+    //Criando MATRIZ
+    for (i = 0 ; i < MAX ; i++){
+        matriz_tab[i] = new int[10];
+    }
 
     //Preenchendo MATRIZ
     for (i = 0 ; i < MAX ; i++){
@@ -22,19 +27,46 @@ void Tabuleiro (){
             matriz_tab[i][j] = 0;
     }
 
-    //Mostrando matriz
+    cout << "    A B C D E F G H I J" << endl;
+    cout << "    -------------------" << endl;
+    
+    //Mostrando matriz 
     for (i = 0 ; i < MAX ; i++){
+        cout << (i) << "  |";
         for (j = 0 ; j < MAX ; j++)
             cout << matriz_tab[i][j] << " ";
         cout << endl;
     }
 
-    //matriz_tab = new int *[10]*[10];
+
+}
+
+void Jogador(int **&matriz_tab){
+
+    int i, j;
+
+    cout << "Jogador 1 posicione seus barcos: " << endl;
+    cout << "posicionar porta aviões (5);" << endl;
+    cout << "linha: ";
+    cin >> i;
+    cout << "coluna: ";
+    cin >> j;
+
+    matriz_tab[i][j] = 5;
+    //Mostrando matriz 
+    for (i = 0 ; i < MAX ; i++){
+        cout << (i) << "  |";
+        for (j = 0 ; j < MAX ; j++)
+            cout << matriz_tab[i][j] << " ";
+        cout << endl;
+    }
 
 
 }
 
 int main(){
+
+    int **matriz = nullptr;
 
     cout << "#################################" << endl;
     cout << "######### BATALHA NAVAL #########" << endl;
@@ -43,7 +75,8 @@ int main(){
     cout << "# Aluna: Rosângela D'Avilla     #" << endl;
     cout << "#################################" << endl;
 
-    Tabuleiro();
+    Tabuleiro(matriz);
     //GERANDO TABULEIRO
+    Jogador(matriz);
     return 0;
 }
